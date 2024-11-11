31389204fafb45816ad0e6dec60f43bb106a35bc
1 <vasilijnenastev@gmail.com>
2024-11-11 22:44:01 +0400

    HW13.1

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,23 @@
-public class Main {   public static String checkingTheYea(int year) {
-    if (year < 1854) {
-        return " год не может быть меньше 1854";
-    }if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-        return year + " - високосный год";
-    } else {
-        return year + " - не високосный год";
+public class Main {
+    public static String calculationLeapYear (int year) {
+        if (year < 1854) {
+            return " год не может быть меньше 1854";
+        }
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            return year + " - високосный год";
+        } else {
+            return year + " - не високосный год";
+        }
     }
-}
 
-    public static String clientDeviceYear(int clientOS1, int year) {
+    public static String clientDeviceYear(int clientOS, int year) {
         int iOS1 = 0;
         int android1 = 1;
-        if (clientOS1 == iOS1 && year >= 2015) {
+        if (clientOS == iOS1 && year >= 2015) {
             return "Установите версию приложения для iOS по ссылке";
-        } else if (clientOS1 == iOS1 && year < 2015) {
+        } else if (clientOS == iOS1 && year < 2015) {
             return "Установите облегченную версию приложения для iOS по ссылке";
-        } else if (clientOS1 == android1 && year >= 2015) {
+        } else if (clientOS == android1 && year >= 2015) {
             return "Установите версию приложения для Android по ссылке";
         } else {
             return "Установите облегченную версию приложения для Android по ссылке";
@@ -40,19 +42,17 @@ public class Main {   public static String checkingTheYea(int year) {
     public static void main(String[] args) {
         System.out.print(" Методы задача №1");
         System.out.println();
-        System.out.println(checkingTheYea(1800));
+        System.out.println(calculationLeapYear(1855));
         System.out.println();
 
         System.out.print(" Методы задача №2");
         System.out.println();
-        System.out.println(clientDeviceYear(1, 2016));
+        System.out.println(clientDeviceYear(1, 2000));
         System.out.println();
 
         System.out.print(" Методы задача №3");
         System.out.println();
-        System.out.println(deliveryDistance(20));
-
-
+        System.out.println(deliveryDistance(200));
 
 
     }
